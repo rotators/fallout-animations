60adcb5908c242f958a944771500de9ce71ff8a0
Wipe <wipe.git@gmail.com>
2021-06-11 22:48:58 +0200

    - provide static versions of each .fr* using `legacy` generator (for now)   with that, all existing frm2png generators are in use - minor changes to script for faster testing

--- frm2pages.sh
+++ frm2pages.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+readonly dir_output=docs
 readonly frm2png=tools/frm2png/Build/frm2png
 
 if [[ ! -f "$frm2png" ]]; then
@@ -14,13 +15,12 @@ fi
 
 # while frm2png can process multiple files at once,
 # it can't really be used here as output is intended to be in different directory with normalized filename
-
 function png-name()
 {
     local frm="$1"
-    local dir=
+    local suffix=
     if (( $# > 1 )); then
-       dir="_$2"
+       suffix="$2"
     fi
 
      frm="${frm//\.\//}"
@@ -28,46 +28,56 @@ function png-name()
      frm_base="$(basename "$frm")"
      frm_name="${frm_base%.*}"
 
-     echo "docs/$frm_dir/${frm_name^^}${dir}.png"
+     echo "$dir_output/$frm_dir/${frm_name^^}${suffix}.png"
 }
 
 #
 
-rm -fr docs
-
-#
-
-find . -iname "*.frm" | sort | while read frm; do
-
-     echo "::group::${frm//\.\//}"
-
-     mkdir -p "docs/$(dirname "$frm")"
-     $frm2png -g anim -o "$(png-name "$frm")" "$frm"
-     $frm2png -g anim-packed -o "$(png-name "$frm")" "$frm" > /dev/null
-
-     echo ::endgroup::
-done
-
-#
-
-for dir in $(seq 0 5); do
-    find . -iname "*.fr$dir" | sort | while read frm; do
+function process-frm()
+{
+    find . -iname "*.frm" | sort | while read frm; do
          echo "::group::${frm//\.\//}"
 
-         mkdir -p "docs/$(dirname "$frm")"
-         $frm2png -g anim -o "$(png-name "$frm" $dir)" "$frm"
+         mkdir -p "$dir_output/$(dirname "$frm")"
+         $frm2png -g anim -o "$(png-name "$frm")" "$frm"
+         # >/dev/null, so frm info will be displayed only once
+         $frm2png -g anim-packed -o "$(png-name "$frm")" "$frm" > /dev/null
+         $frm2png -g legacy -o "$(png-name "$frm" .static)" "$frm" > /dev/null
 
          echo ::endgroup::
     done
-
-    find docs/ -iname "*_${dir}_0.png" | while read png; do
-         png_new="${png//_${dir}_0/_${dir}}"
-         mv "$png" "$png_new"
-    done
-done
+}
 
 #
 
-cd docs
+function process-frX()
+{
+    for dir in $(seq 0 5); do
+        find . -iname "*.fr$dir" | sort | while read frm; do
+             echo "::group::${frm//\.\//}"
 
-tree -H . -P *.png > index.html
+             mkdir -p "$dir_output/$(dirname "$frm")"
+             $frm2png -g anim -o "$(png-name "$frm" _$dir)" "$frm"
+             # >/dev/null, so frm info will be displayed only once
+             $frm2png -g legacy -o "$(png-name "$frm" _$dir.static)" "$frm" > /dev/null
+
+             echo ::endgroup::
+        done
+
+        find $dir_output/ -iname "*_${dir}_0.png" | while read png; do
+             png_new="${png//_${dir}_0/_${dir}}"
+             mv "$png" "$png_new"
+        done
+    done
+}
+
+#
+
+rm -fr $dir_output
+
+process-frm
+process-frX
+
+(cd $dir_output && tree --sort=name -H . -P *.png > index.html)
+echo
+du -hd0 $dir_output/
